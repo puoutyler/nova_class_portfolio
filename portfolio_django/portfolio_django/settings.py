a5d3d807c7d51861c3ddd1b05be784f507e19755
Tyler Puou <tylerpuou@Shalenes-MBP.fios-router.home>
2020-06-25 08:03:20 -0700

    added rewquiremnts and gitignore

--- portfolio_django/portfolio_django/settings.py
+++ portfolio_django/portfolio_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@^uy2%hvlupgrk#ao#_!p+)4_t08d#(-pav4og1gc)1&@+2ybj'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['167.172.211.100', '127.0.0.1', 'localhost', 'tylerpuou.com']
 
 
 # Application definition
